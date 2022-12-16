a642cafbbe12e566c9bcad531aaff472023a0bc3
Wolodja <wolodjawaskovych@i.ua>
2022-12-16 17:04:35 +0100

    Prepared project panel

--- src/index.js
+++ src/index.js
@@ -5,20 +5,31 @@ const resolver = new Resolver();
 const requiredSummary = 50;
 const requiredDescription = 4;
 
+function getScore(data) {
+    const summary = data.fields.summary;
+    let summaryScore = summary ? summary.length / requiredSummary : 0;
+    if (summaryScore > 1) {
+        summaryScore = 1;
+    }
+    const description = data.fields.description;
+    let descriptionScore = description ? description.content.length / requiredDescription : 0;
+    if (descriptionScore > 1) {
+        descriptionScore = 1;
+    }
+    return (summaryScore + descriptionScore) / 2 * 100;
+}
+
 resolver.define('getText', async (req) => {
     console.log(req);
     console.log(`Issue key ${req.context.extension.issue.key}`);
     const response = await api.asApp().requestJira(route`/rest/api/3/issue/${req.context.extension.issue.key}`)
     const data = await response.json();
-    let summaryScore = data.fields.summary.length / requiredSummary;
-    if (summaryScore > 1) {
-        summaryScore = 1;
-    }
-    let descriptionScore = data.fields.description.content.length / requiredDescription;
-    if (descriptionScore > 1) {
-        descriptionScore = 1;
-    }
-    return (summaryScore + descriptionScore) / 2 * 100;
+    return getScore(data);
+});
+
+resolver.define('getProjectOverview', async (req) => {
+    console.log(req);
+    return "Project overview";
 });
 
 export const handler = resolver.getDefinitions();
